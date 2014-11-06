e1b886812068d172a16878ba3a86afb94d511988
ch5766 <choiwy2001@naver.com>
2014-11-06 15:08:54 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
-var bg, img;
+var bg, img1, img2, img3;
 
 function setup() 
 {
    createCanvas(1000,1000);
    bg = loadImage("galaxy.jpg");
-   img = loadImage("earth.jpg");
+   img1 = loadImage("earth.png");
+   img2 = loadImage("moon.png");
+   img3 = loadImage("sun.png");
+   img4 = loadImage("ura.png");
 }
 
 function draw() 
 {
    background(bg);
    noStroke();
-   fill(255,139,0);
-   ellipse(500,500,150,150);
+   image(img3,500,500,150,150);
 
    //earth
    var duration = 3000;
    var timing = (new Date()%duration)/duration;
 
-   image(img, 500 + Math.cos(timing*2*PI)*200,           // x좌표
+   image(img1, 500 + Math.cos(timing*2*PI)*200,           // x좌표
         500 + Math.sin(timing*2*PI)*200, // y좌표
         100,                                  // width
         100);            
@@ -27,19 +29,18 @@ function draw()
    var duration = 1000;
    var timing2 = (new Date()%duration)/duration;
 
-   fill(255,206,0);
-   ellipse(500 + Math.cos(timing*2*PI)*200+ Math.cos(timing2*2*PI)*70,           // x좌표
+   image(img2, 500 + Math.cos(timing*2*PI)*200+ Math.cos(timing2*2*PI)*70,           // x좌표
         500 + Math.sin(timing*2*PI)*200+ Math.cos(timing2*2*PI)*70, // y좌표
         30,                                  // width
         30);   
                                       // height
 
-   //star
-    var duration = 5000;
+   //ura
+   var duration = 5000;
    var timing = (new Date()%duration)/duration;
 
    fill(65,77,181);
-   ellipse(500 + Math.cos(timing*2*PI)*300+ Math.cos(timing*2*PI)*100,           // x좌표
+   image(img4, 500 + Math.cos(timing*2*PI)*300+ Math.cos(timing*2*PI)*100,           // x좌표
         500 + Math.sin(timing*2*PI)*300+ Math.cos(timing*2*PI)*100, // y좌표
         60,                                  // width
         60);                                  
