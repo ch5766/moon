c12c0475ff527bd25f822282cfcee45d321582a3
ch5766 <choiwy2001@naver.com>
2014-11-06 14:44:12 +0900

    dd
    dd

---
+++ sketch.js
@@ -0,0 +1,45 @@
+function setup() 
+{
+   createCanvas(1000,1000);
+   bg = loadImage("galaxy.jpg");
+
+}
+
+function draw() 
+{
+   background(bg);
+   noStroke();
+   fill(255,139,0);
+    ellipse(500,500,150,150);
+
+   //earth
+   var duration = 3000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(151,239,172);
+   ellipse(500 + Math.cos(timing*2*PI)*200,           // x좌표
+        500 + Math.sin(timing*2*PI)*200, // y좌표
+        100,                                  // width
+        100);            
+
+   //moon
+   var duration = 1000;
+   var timing2 = (new Date()%duration)/duration;
+
+   fill(255,206,0);
+   ellipse(500 + Math.cos(timing*2*PI)*200+ Math.cos(timing2*2*PI)*70,           // x좌표
+        500 + Math.sin(timing*2*PI)*200+ Math.cos(timing2*2*PI)*70, // y좌표
+        30,                                  // width
+        30);   
+                                      // height
+
+   //star
+    var duration = 5000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(65,77,181);
+   ellipse(500 + Math.cos(timing*2*PI)*300+ Math.cos(timing*2*PI)*100,           // x좌표
+        500 + Math.sin(timing*2*PI)*300+ Math.cos(timing*2*PI)*100, // y좌표
+        60,                                  // width
+        60);                                  
+}
